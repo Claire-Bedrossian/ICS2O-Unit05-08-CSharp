de0a644c4c32fbc61b1699fef0025d5abe46a3d2
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-12-11 14:34:21 +0000

    added function

--- main.cs
+++ main.cs
@@ -1,7 +1,35 @@
 using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Random random = new System.Random();
+        // This function does division
+        double numA;
+        double numB;
+        double counter = 0;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+        Console.Write("This program does division.");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.Write("Formula: a / b = c");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.Write("Enter value a:");
+        numA = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter value b:");
+        numB = Convert.ToDouble(Console.ReadLine());
+
+        double remainder = numA;
+
+        while (remainder >= numB)
+        {
+            remainder = remainder - numB;
+            counter++;
+        }
+        Console.WriteLine("");
+        Console.Write("The sum is " + counter + " R " + remainder + ".");
+        Console.WriteLine("");
+        Console.Write("/Done");
+    }
 }
